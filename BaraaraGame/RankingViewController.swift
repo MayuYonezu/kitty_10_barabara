6f1a8ac6e0bb667b047bd521df6512001798224b
MayuYonezu <s2122054@stu.musashino-u.ac.jp>
2022-05-12 22:21:31 +0900

    Initial Commit

---
+++ BaraaraGame/RankingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BaraaraGame
+//
+//  Created by Mayu Yonezu on 2022/05/09.
+//
+
+import UIKit
+
+class RankingViewController: UIViewController {
+    
+    @IBOutlet var rankingLabel1: UILabel!
+    @IBOutlet var rankingLabel2: UILabel!
+    @IBOutlet var rankingLabel3: UILabel!
+    
+    let defaults: UserDefaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        rankingLabel1.text = String(defaults.integer(forKey: "score1"))
+        rankingLabel2.text = String(defaults.integer(forKey: "score2"))
+        rankingLabel3.text = String(defaults.integer(forKey: "score3"))
+    }
+    
+    @IBAction func toTop(){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
